5accce6978fd7a2341d2c365bb37bb2f84fb871f
ericcapiz <ericcapiz@gmail.com>
2021-04-02 17:09:04 -0500

    added media-queries

--- src/components/Infobox/Infobox.js
+++ src/components/Infobox/Infobox.js
@@ -1,10 +1,11 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
 import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
+import './Infobox.css';
 
 const Infobox = ({title, cases, total}) => {
     return (
         <Card className="infoBox">
-            <CardContent className="cardContain">
+            <CardContent>
                 <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
                 <h2 className="infoBox__cases">{cases}</h2>
                 <Typography className="infoBox__total" color="textSecondary">Total: {total}</Typography>
